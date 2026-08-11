f0f361ec5468028d16608b8e8f52bace6ac1548c
Manvesh Singh <168457140+manvesh12@users.noreply.github.com>
2026-08-11 23:50:54 +0530

    Solved 1456. Maximum Number of Vowels in a Substring of Given Length

--- leetcode/1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ leetcode/1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -6,33 +6,40 @@ class Solution {
         int tempcount = 0;
         int count = 0;
 
-        while (j < n) {
-
-            if (i <= j) {
-
-                if (s.charAt(i) == 'a' || s.charAt(i) == 'e' ||
-                    s.charAt(i) == 'i' || s.charAt(i) == 'o' ||
-                    s.charAt(i) == 'u') {
-                    tempcount++;
-                }
-
-                i++;
-            } 
-            else {
-                if (tempcount > count) {
-                    count = tempcount;
-                }
-
-                // next overlapping window
-                i = i - k + 1;
-                j = i + k - 1;
-
-                tempcount = 0;
+        // First window
+        while (i <= j) {
+            if (s.charAt(i) == 'a' || s.charAt(i) == 'e' ||
+                s.charAt(i) == 'i' || s.charAt(i) == 'o' ||
+                s.charAt(i) == 'u') {
+                tempcount++;
             }
+            i++;
         }
 
-        if (tempcount > count) {
-            count = tempcount;
+        count = tempcount;
+
+        // Next windows
+        i = 1;
+        j = i + k - 1;
+
+        while (j < n) {
+
+            if (s.charAt(i - 1) == 'a' || s.charAt(i - 1) == 'e' ||
+                s.charAt(i - 1) == 'i' || s.charAt(i - 1) == 'o' ||
+                s.charAt(i - 1) == 'u') {
+                tempcount--;
+            }
+
+            if (s.charAt(j) == 'a' || s.charAt(j) == 'e' ||
+                s.charAt(j) == 'i' || s.charAt(j) == 'o' ||
+                s.charAt(j) == 'u') {
+                tempcount++;
+            }
+
+            count = Math.max(count, tempcount);
+
+            i++;
+            j++;
         }
 
         return count;
