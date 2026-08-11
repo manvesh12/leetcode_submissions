acf069ebb013c00727ddc9ea368c8a8ed7c85635
Manvesh Singh <168457140+manvesh12@users.noreply.github.com>
2026-08-11 15:52:24 +0530

    Solved 125. Valid Palindrome

--- leetcode/125-valid-palindrome/125-valid-palindrome.java
+++ leetcode/125-valid-palindrome/125-valid-palindrome.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isPalindrome(String s) {
         boolean c = true;
-        s.trim();
+        
         
         int i = 0;
         int j = s.length()-1;
         while(i<j){
-            while(!Character.isLetterOrDigit(s.charAt(i))){
+            while(i<j && !Character.isLetterOrDigit(s.charAt(i))){
                 i++;
             }
-            while(!Character.isLetterOrDigit(s.charAt(j))){
+            while(i<j &&!Character.isLetterOrDigit(s.charAt(j))){
                 j--;
             }
             if(s.toLowerCase().charAt(i)==s.toLowerCase().charAt(j)){
