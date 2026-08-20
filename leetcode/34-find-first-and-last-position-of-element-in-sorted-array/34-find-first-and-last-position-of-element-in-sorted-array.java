c3644d92433eef70fa39a406c0f7668213ce1be7
Manvesh Singh <168457140+manvesh12@users.noreply.github.com>
2026-08-21 00:03:21 +0530

    Solved 34. Find First and Last Position of Element in Sorted Array

--- leetcode/34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ leetcode/34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,71 +1,42 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-
         int n = nums.length;
+        int low = 0 ;
+         int high = n-1 ;
+         int start = -1;
+          int end = -1;
+         
 
-        int start = -1;
-        int end = -1;
 
-        int low = 0;
-        int high = n - 1;
-
-        int[] arr = new int[2];
-
-        // Find first occurrence
-        while (low <= high) {
-
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
-
+         while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target ){
                 start = mid;
-
-                // Search on the left
-                high = mid - 1;
-
-            } 
-            else if (nums[mid] < target) {
-
-                low = mid + 1;
-
-            } 
-            else {
-
-                high = mid - 1;
+                 high = mid-1;
             }
-        }
+            else if(nums[mid]<target){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+         }
 
-        // Reset binary search
-        low = 0;
-        high = n - 1;
-
-        // Find last occurrence
-        while (low <= high) {
-
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
-
+         low = 0;
+         high = n - 1;
+          while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target ){
                 end = mid;
-
-                // Search on the right
-                low = mid + 1;
-
-            } 
-            else if (nums[mid] < target) {
-
-                low = mid + 1;
-
-            } 
-            else {
-
-                high = mid - 1;
+                low = mid+1;
             }
-        }
-
-        arr[0] = start;
-        arr[1] = end;
-
-        return arr;
+           else if(nums[mid]<target){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+         }
+          return new int []{start,end};
     }
 }
