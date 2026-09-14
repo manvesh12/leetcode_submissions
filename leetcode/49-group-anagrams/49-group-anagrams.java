8000e5c0269d669e888a65de05943be1c51bc3ff
Manvesh Singh <168457140+manvesh12@users.noreply.github.com>
2026-09-14 23:02:34 +0530

    Solved 49. Group Anagrams

--- leetcode/49-group-anagrams/49-group-anagrams.java
+++ leetcode/49-group-anagrams/49-group-anagrams.java
@@ -1,16 +1,15 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<HashMap<Character,Integer>,List<String>> map = new HashMap<>();
-        for(String s :strs){
-            HashMap<Character,Integer> h1 = new HashMap<>();
-            for( char ch : s.toCharArray()){
-              h1.put(ch,h1.getOrDefault(ch,0)+1);
+        for(String s : strs){
+        HashMap<Character,Integer> h1 = new HashMap<>();
+            for(char ch : s.toCharArray()){
+                h1.put(ch,h1.getOrDefault(ch,0)+1);
             }
             if(!map.containsKey(h1)){
                 map.put(h1,new ArrayList<>());
             }
-                map.get(h1).add(s);
-            
+            map.get(h1).add(s);
         }
         return new ArrayList<>(map.values());
     }
