152abb1a5beb3779b6101baaefc288a4e49b35e4
Manvesh Singh <168457140+manvesh12@users.noreply.github.com>
2026-09-14 22:52:48 +0530

    Solved 49. Group Anagrams

--- leetcode/49-group-anagrams/49-group-anagrams.java
+++ leetcode/49-group-anagrams/49-group-anagrams.java
@@ -1,47 +1,17 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        int  n = strs.length;
-        boolean  [] visited = new boolean[n];
-        List<List<String>> a1 = new ArrayList<>();
-        for( int i = 0 ;i<n;i++){
-            if(visited[i]){
-                continue;
+        HashMap<HashMap<Character,Integer>,List<String>> map = new HashMap<>();
+        for(String s :strs){
+            HashMap<Character,Integer> h1 = new HashMap<>();
+            for( char ch : s.toCharArray()){
+              h1.put(ch,h1.getOrDefault(ch,0)+1);
             }
-            HashMap<Character,Integer> h1 = frequency(strs[i]);
-            ArrayList<String> arr = new ArrayList<>();
-            arr.add(strs[i]);
-            visited[i]= true;
-
-            for( int j = i+1;j<n;j++){
-                if(visited[j]){
-                continue;
+            if(!map.containsKey(h1)){
+                map.put(h1,new ArrayList<>());
             }
+                map.get(h1).add(s);
             
-            HashMap<Character,Integer> h2 = frequency(strs[j]);
-            
-            if(h1.equals(h2)){
-                    visited[j]= true;
-                    arr.add(strs[j]);
-            }
-            }
-
-a1.add(arr); 
-            }
-          
-return a1   ;
         }
-        public HashMap<Character, Integer> frequency(String s) {
-
-    HashMap<Character, Integer> h = new HashMap<>();
-
-    for (int i = 0; i < s.length(); i++) {
-
-        char ch = s.charAt(i);
-
-        h.put(ch, h.getOrDefault(ch, 0) + 1);
+        return new ArrayList<>(map.values());
     }
-
-    return h;
 }
-        
-    }
