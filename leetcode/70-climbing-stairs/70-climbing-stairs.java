0b1d3ffaa50b0e76ada4812cda53768a6192757e
Manvesh Singh <168457140+manvesh12@users.noreply.github.com>
2026-09-25 23:43:25 +0530

    Solved 70. Climbing Stairs

--- leetcode/70-climbing-stairs/70-climbing-stairs.java
+++ leetcode/70-climbing-stairs/70-climbing-stairs.java
@@ -7,7 +7,9 @@ class Solution {
     public int solve (int n ,int []arr){
         int sum = 0 ;
         if(n<2) return 1;
+        if(arr[n]!=0 )return arr[n];
         sum = solve(n-1,arr)+solve(n-2,arr);
+        arr[n] = sum;
         return sum;
     }
 }
