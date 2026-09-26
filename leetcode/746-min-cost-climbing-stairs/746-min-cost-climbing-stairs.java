f0541835097d8181740e520784d596ecf5ceef89
Manvesh Singh <168457140+manvesh12@users.noreply.github.com>
2026-09-26 12:55:14 +0530

    Solved 746. Min Cost Climbing Stairs

---
+++ leetcode/746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int [] dp = new int [n];
+        return Math.min(solve(0,cost,dp),solve(1,cost,dp));
+    }
+    public int solve(int a ,int[] cost,int [] dp ){
+        if(a>=dp.length) return 0;
+        if(dp[a]!=0) return dp[a];
+        int mincost = cost[a]+ Math.min(solve(a+1,cost,dp),solve(a+2,cost,dp));
+    dp[a]= mincost;
+    return mincost;
+    }
+}
